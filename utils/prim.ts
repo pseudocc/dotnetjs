be72a0f5086e00ae8d462e08fb149771dc5763a0
Master76 <15811297278@163.com>
2016-10-24 16:28:33 +0800

    say hello to the console

--- utils/prim.ts
+++ utils/prim.ts
@@ -4,4 +4,17 @@
         Dispose(): void;
     }
 
+    function GetVersion(): string {
+        var Major = 1;
+        var Build = 2;
+        var Revision = 2;
+        
+        return Major + '.' + Build + '.' + Revision;
+    }
+
+    export function Greetings() {
+        var version = GetVersion();
+        console.log('DotNetJs -', version, '- https://github.com/Master76/dotnetjs/ -');
+    }
+
 }
