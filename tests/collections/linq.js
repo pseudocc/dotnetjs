a0ef559beed2c469850af654603b89ba34c0d577
Master76 <15811297278@163.com>
2016-10-28 17:47:24 +0800

    update linq.test

--- tests/collections/linq.js
+++ tests/collections/linq.js
@@ -16,8 +16,21 @@ list.Add({ foo: index++, bar: '__2', wtf: false });
 var seed = 0;
 var expression = Linq.LinqStart(list);
 seed = expression.Aggregate(seed, function (acc, item) { return (acc + item.foo); });
-console.log('seed:', seed); // seed: 36
-expression.Where(function (item) { return item.bar === '__1' && !item.wtf; }).ForEach(function (item) { return console.log(item.GetHashCode()); }); //  0\n 1\n
+console.log('seed:', seed);
+// seed: 36
+expression.Where(function (item) { return item.bar === '__1' && !item.wtf; }).ForEach(function (item) { return console.log(item.GetHashCode()); });
+// 0
+// 1
 var avg = expression.Select(function (item) { return item.foo; }).Where(function (item) { return item <= 5; }).Average();
-console.log('Average:', avg); // Average: 2.5
-expression.SkipWhile(function (item, index) { return item.bar === '__0' || index > 5; }).ForEach(function (item) { return console.log(item.foo, item.bar); }); // 3 "__1"\n 4 "__1"\n 5 "__1"\n
+console.log('Average:', avg);
+// Average: 2.5
+expression.SkipWhile(function (item, index) { return item.bar === '__0' || index > 5; }).ForEach(function (item) { return console.log(item.foo, item.bar); });
+// 3 "__1"
+// 4 "__1"
+// 5 "__1"
+expression.Where(function (item) { return item.wtf; }).ToDictionary(function (item) {
+    return { Key: item.foo, Value: item.bar };
+}).ForEach(function (item) { return console.log('key:', item.Key + ',', 'value:', item.Value); });
+// key: 2, value: __0
+// key: 3, value: __1
+// key: 7, value: __2
--- tests/collections/linq.ts
+++ tests/collections/linq.ts
@@ -26,11 +26,25 @@ list.Add({ foo: index++, bar: '__2', wtf: false });
 var seed = 0;
 var expression = Linq.LinqStart(list);
 seed = expression.Aggregate(seed, (acc, item) => (acc + item.foo));
-console.log('seed:', seed); // seed: 36
+console.log('seed:', seed); 
+// seed: 36
 
-expression.Where(item => item.bar === '__1' && !item.wtf).ForEach(item => console.log(item.GetHashCode())); //  0\n 1\n
+expression.Where(item => item.bar === '__1' && !item.wtf).ForEach(item => console.log(item.GetHashCode())); 
+// 0
+// 1
 
 var avg = expression.Select(item => item.foo).Where(item => item <= 5).Average();
-console.log('Average:', avg);// Average: 2.5
+console.log('Average:', avg);
+// Average: 2.5
 
-expression.SkipWhile((item, index) => item.bar === '__0' || index > 5).ForEach(item => console.log(item.foo, item.bar)); // 3 "__1"\n 4 "__1"\n 5 "__1"\n
+expression.SkipWhile((item, index) => item.bar === '__0' || index > 5).ForEach(item => console.log(item.foo, item.bar)); 
+// 3 "__1"
+// 4 "__1"
+// 5 "__1"
+
+expression.Where(item => item.wtf).ToDictionary((item) => {
+    return {Key: item.foo, Value: item.bar};
+}).ForEach((item) => console.log('key:', item.Key + ',', 'value:', item.Value));
+// key: 2, value: __0
+// key: 3, value: __1
+// key: 7, value: __2
