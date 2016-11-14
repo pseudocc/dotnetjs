777c790ddfa4182993cdea0baced0138fd087743
Master76 <15811297278@163.com>
2016-11-14 18:14:59 +0800

    extension for console, writeline implemented

---
+++ utils/cslext.ts
@@ -0,0 +1,16 @@
+/// <reference path="strext.ts" />
+
+interface Console {
+    writeLine(format?: string, ...args: any[]);
+}
+
+(function() {
+    console.writeLine = function(format?: string) {
+        if (format == null) {
+            console.log();
+            return;
+        }
+        var msg = String.Format.apply(null, arguments);
+        console.log(msg);
+    }
+})();
