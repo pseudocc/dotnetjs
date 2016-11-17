8d5fd86a8e63f59469d0fb80368e4c9948a5a526
Master76 <15811297278@163.com>
2016-11-17 15:55:14 +0800

    test cases now return with exit code.

--- tests/utils/char.ts
+++ tests/utils/char.ts
@@ -1,11 +1,12 @@
 import DotnetJs = require('dotnetjs');
 var char = DotnetJs.Char;
 
-export default function runTest(assertEqual: (expect: Object, result: Object) => void) {
+export default function runTest(assertEqual: (expect: Object, result: Object) => void): number {
     assertEqual(false, char.IsUpper('Foo', 1));
     assertEqual(true, char.IsPunctuation(','));
     assertEqual(true, char.IsControl('\r'));
     assertEqual(true, char.IsDigit('7'));
     assertEqual(true, char.IsLetter('bar', 2));
     console.log('char test completed successully.');
+    return 0;
 }
