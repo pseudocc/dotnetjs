8dc49004e4e1a84c0cdc0bcc47e042bac53346ff
Master76 <15811297278@163.com>
2016-11-23 15:19:33 +0800

    remove lic in exported js file

--- dist/dotnet.js
+++ dist/dotnet.js
@@ -1,20 +1,3 @@
-/**
- *
- *  The MIT License (MIT)
- *  Copyright (c) 2016 Master Yu
- *  
- *  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), 
- *  to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, 
- *  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- *  
- *  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- *  
- *  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
- *  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
- *  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
- *  IN THE SOFTWARE.
- *
-**/
 var __extends = (this && this.__extends) || function (d, b) {
     for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p];
     function __() { this.constructor = d; }
@@ -118,9 +101,6 @@ var DotnetJs;
 (function () {
     var id = 0x7FEFFFFD;
     function StringHash(obj) {
-        if (obj.IsValueType) {
-            throw new DotnetJs.NotImplementedExeption('GetHashCode(boolean)');
-        }
         var value = obj.toString();
         var m, n = 0x15051505;
         var offset = 0;
@@ -1125,7 +1105,7 @@ var DotnetJs;
     }());
     DotnetJs.DefaultDelegate = DefaultDelegate;
     function GetVersion() {
-        return new DotnetJs.Version(1, 5, 11, 49);
+        return new DotnetJs.Version(1, 5, 12, 51);
     }
     function Greetings() {
         var version = GetVersion();
