ba68e411ad7c0d1d5a0e986ff2183de322d3a309
Master76 <15811297278@163.com>
2017-05-04 18:05:38 +0800

    update utest for linq

--- tests/collections/linq.ts
+++ tests/collections/linq.ts
@@ -8,7 +8,10 @@ export default function runTest(assertEqual: (expect: Object, result: Object) =>
     assertEqual(true, Linq.SequenceEqual([56, 63, 66, 99, 52], above50));
     assertEqual(true, Linq.SequenceEqual(toArr, above50));
     assertEqual(true, Linq.SequenceEqual(Linq.Repeat(0, 5), [0, 0, 0, 0, 0]));
+    assertEqual(Linq.Concat([23, 42, 44], [11, 33]).ToArray(), [23, 42, 44, 11, 33]);
+    assertEqual(Linq.Except([23, 42, 44, 14, 52], [11, 42, 14]).ToArray(), [23, 44, 52]);
     assertEqual(99, toArr[3]);
+    assertEqual(99, above50.ElementAt(3));
     console.log('linq test completed successully.');
     return 0;
 }
