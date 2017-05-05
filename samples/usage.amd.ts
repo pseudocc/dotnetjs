de8a4cf56ba778b8d7d891e77e4c956a83880570
Master76 <15811297278@163.com>
2017-05-05 14:44:21 +0800

    update samples for Dictionary.TryGetValue

--- samples/usage.amd.ts
+++ samples/usage.amd.ts
@@ -7,12 +7,17 @@ type LocalKey = {
 }
 
 var ht = new HashTable<LocalKey, string>();
-ht.Add({ value: 'cat' }, 'animal');
+var key_cat = { value: 'cat' };
+ht.Add(key_cat, 'animal');
 ht.Add({ value: 'rat' }, 'animal');
 ht.Add({ value: 'dog' }, 'animal');
 ht.Add({ value: 'tree' }, 'plant');
 ht.Add({ value: 'bushes' }, 'plant');
 ht.Add({ value: 'car' }, 'machine');
+
+var type;
+if (ht.TryGetValue(key_cat, out => type = out))
+    console.log(type);
 
 var expression = Linq.LinqStart(ht);
 expression.Where((item) => item.Value == 'animal').Select((item) => item.Key.value).ForEach((item) => console.log(item));
