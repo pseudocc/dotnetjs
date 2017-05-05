9d11a5c7f7b8ea9a2b81b1a65733f7623075f05d
Master76 <15811297278@163.com>
2017-05-05 14:33:35 +0800

    update ut

--- tests/collections/list.ts
+++ tests/collections/list.ts
@@ -10,13 +10,18 @@ l1.Add(7);
 l1.Add(4);
 
 var l2 = new List<number>([0, 1, 8, 7, 4]);
+var l3 = new List<number>([0, 1, 2, 3, 4, 5, 6, 7, 8, 9]);
+l3.RemoveAll(item => item < 5);
 
 export default function runTest(assertEqual: (expect: Object, result: Object) => void): number {
     assertEqual(true, Linq.SequenceEqual(l1, l2));
+    assertEqual(true, Linq.SequenceEqual(l3, [5, 6, 7, 8, 9]));
     assertEqual(5, l1.Count);
     assertEqual(3, l1.IndexOf(7));
     l1.Remove(7);
     assertEqual(4, l1.Count);
+    l1.Clear();
+    assertEqual(0, l1.Count);
     console.log('list test completed successully.');
     return 0;
 }
